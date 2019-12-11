bf1d10d6dcb496fd810d6c14ed999cbb4aa539db
ismayilhusein <ismayilrh@code.edu.az>
2019-12-11 06:49:19 -0800

    CreatePages

---
+++ FinalMvcProject/Controllers/BlogController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FinalMvcProject.Controllers
+{
+    public class BlogController : Controller
+    {
+        // GET: Blog
+        public ActionResult List()
+        {
+            return View();
+        }
+        public ActionResult Grid()
+        {
+            return View();
+        }
+        public ActionResult Single()
+        {
+            return View();
+        }
+    }
+}
---
+++ FinalMvcProject/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FinalMvcProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Home2()
+        {
+            return View();
+        }
+        public ActionResult Home3()
+        {
+            return View();
+        }
+    }
+}
