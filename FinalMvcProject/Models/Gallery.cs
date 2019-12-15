dd056085d44e97f8a0f26b333aee302ddda1a3ee
ismayilhusein <ismayilrh@code.edu.az>
2019-12-15 13:05:02 -0800

    Create Blog List

---
+++ FinalMvcProject/Models/Gallery.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FinalMvcProject.Models
+{
+    public class Gallery
+    {
+        public int Id { get; set; }
+        public string Photo { get; set; }
+        public HttpPostedFileBase PhotoUpload { get; set; }
+        public bool Status { get; set; }
+
+    }
+}
